001686f56e2a9a2bbe23417531079dad1054ba5e
Johan Kremer <johan.h.kremer@gmail.com>
2026-04-21 11:59:25 +0200

    Add intial examples for datatypes and varibles

---
+++ script/index.js
@@ -0,0 +1,86 @@
+console.log("--- Datatyper ---")
+console.log('johan =', typeof 'johan')
+console.log('123 =', typeof 123)
+console.log('true =', typeof true)
+console.log("undefined =", typeof undefined)
+console.log("null =", typeof null)
+console.log("[] =", typeof [])
+console.log("{} =", typeof {})
+
+/* Datatyper */
+// string - text
+// number - 123
+// boolean - true/false
+// undefined - inget värde
+// null - inget värde
+// object - {key: value, key: value}
+//----------------------------------
+// bigint - Väldigt stora nummer
+// symbol - Ingnorera just nu
+
+// array - [1, 2, 3, 4, 5] = object
+// functions
+
+/* Varibler */
+
+// var (gammal,  används inte idag)
+// ----- Moderna varibler -----
+// let
+// const
+
+//varibel let = får skriva över data
+let name = "🔥 Johan"
+console.log("--- Variabel let ---")
+console.log(name)
+
+name = "🌊 Stina"
+
+console.log(name)
+
+//varibel const = får INTE skriva över data
+
+const stad = "🌆 Malmö"
+console.log("--- Variabel const ---")
+console.log(stad)
+
+// stad = "🌃 Göteborg"
+
+// console.log(stad)
+
+//! Uncaught TypeError: Assignment to constant variable.
+
+// Array
+const fruits = [
+    "🍍 ananas",
+    "🍊 aplesin",
+    "🍎 äpple",
+    "🥝 kiwi"
+]
+
+const numbersArray = [1, 2, 3, 4, 5]
+
+const mixedArray = ["string", true, 1, null]
+
+console.log("--- Array ----")
+console.log(fruits)
+console.log("Antal frukter = ", fruits.length)
+
+//Object
+const teachers = [
+    {
+        user: "johan",
+        age: 44,
+        course: "Javascript 1",
+        couserActive: true
+    },
+    {
+        user: "stina",
+        age: 33,
+        course: "html/css",
+        couserActive: false
+    }
+]
+
+console.log("--- Object ----")
+console.log(teachers)
+
